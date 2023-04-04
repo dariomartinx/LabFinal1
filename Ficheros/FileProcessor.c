66dd52bb867c34a560a5e489a16b2ccd37c0f00e
Rodrisd <123882426+Rodrisd@users.noreply.github.com>
2023-04-04 16:23:13 +0200

    Update FileProcessor.c

--- Ficheros/FileProcessor.c
+++ Ficheros/FileProcessor.c
@@ -1,48 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 typedef struct conf{
-	char rutaFiles[25];
-	char invFile[25];
-	char logFile[25];
-	int nProc;
-	int simSlp;
+        char rutaFiles[25];
+        char invFile[25];
+        char logFile[25];
+        int nProc;
+        int simSlpMax;
+        int simSlpMin;
+        char rutaBin[25];
 }confstruct;
 
-void *funcionThread (void *parametro);
+void *funcionThread(void *parametro);
 
 int main(){
+int tuberia[2];
+pipe(tuberia);
 char str[50], *tok, tConf[25];
 confstruct configuracion;
-pthread_t idHilo[5];
-
-FILE *pfich = fopen("fp.conf.txt", "r");								//Fichero de configuración
-if(pfich == NULL)
-	printf("Error al abrir el fichero de configuración");
+if(fork()==0){
+        execv("./Monitor", *argv);
+}
 else{
-	while(fgets(str,50,pfich)!=NULL){
-		tok = strtok(str, "=");
-		strcpy(tConf,tok);
-		tok = strtok(NULL,"=");
-		if(strcmp(tConf,"PATH_FILES")==0){
-			strcpy(configuracion.rutaFiles, tok);
-			configuracion.rutaFiles[strlen(configuracion.rutaFiles)-1]='\0';
-		}
-		else if(strcmp(tConf,"INVENTORY_FILE")==0){
-			strcpy(configuracion.invFile, tok);
-			configuracion.invFile[strlen(configuracion.invFile)-1]='\0';
-		}
-		else if(strcmp(tConf,"LOG_FILE")==0){
-                        strcpy(configuracion.logFile, tok);
-			configuracion.logFile[strlen(configuracion.logFile)-1]='\0';
-		}
-		else if(strcmp(tConf,"NUM_PROCESOS")==0)
-                        configuracion.nProc = atoi(tok);
-		else if(strcmp(tConf,"SIMULATE_SLEEP")==0)
-                        configuracion.simSlp = atoi(tok);
-	}
-	fclose(pfich);
+        FILE *pfich = fopen("fp.conf.txt", "r");                        //Fichero de configuración
+        if(pfich == NULL){
+                printf("Error al abrir el fichero de configuración");
+                dup2(tuberia[1],STDOUT_FILENO);
+        }
+        else{
+                printf("Fichero de configuración abierto correctamente");
+                dup2(tuberia[1],STDOUT_FILENO);
+                while(fgets(str,50,pfich)!=NULL){
+                        tok = strtok(str, "=");
+                        strcpy(tConf,tok);
+                        tok = strtok(NULL,"=");
+                        if(strcmp(tConf,"PATH_FILES")==0){
+                                strcpy(configuracion.rutaFiles, tok);
+                                configuracion.rutaFiles[strlen(configuracion.rutaFiles)-1]='\0';
+                        }
+                        else if(strcmp(tConf,"INVENTORY_FILE")==0){
+                                strcpy(configuracion.invFile, tok);
+                                configuracion.invFile[strlen(configuracion.invFile)-1]='\0';
+                        }
+                        else if(strcmp(tConf,"LOG_FILE")==0){
+                                strcpy(configuracion.logFile, tok);
+				configuracion.invFile[strlen(configuracion.invFile)-1]='\0';
+                        }
+                        else if(strcmp(tConf,"LOG_FILE")==0){
+                                strcpy(configuracion.logFile, tok);
+                                configuracion.logFile[strlen(configuracion.logFile)-1]='\0';
+                        }
+                        else if(strcmp(tConf,"NUM_PROCESOS")==0)
+                                configuracion.nProc = atoi(tok);
+                        else if(strcmp(tConf,"SIMULATE_SLEEP_MAX")==0)
+                                configuracion.simSlpMax = atoi(tok);
+                        else if(strcmp(tConf,"SIMULATE_SLEEP_MIN")==0)
+                                configuracion.simSlpMin = atoi(tok);
+                        else if(strcmp(tConf,"PATH_BIN")==0){
+                                strcpy(configuracion.rutaBin, tok);
+                                configuracion.rutaBin[strlen(configuracion.rutaBin)-1]='\0';
+                        }
+                }
+                fclose(pfich);
+        }
+while(1){}
+}
 }
 
-}
