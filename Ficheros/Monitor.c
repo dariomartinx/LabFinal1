b1cdbd311bedd3f64a2001a2cd7805027580b4b5
dariomartinx <101869065+dariomartinx@users.noreply.github.com>
2023-04-14 10:45:28 +0200

    Update Monitor.c

--- Ficheros/Monitor.c
+++ Ficheros/Monitor.c
@@ -1,24 +1,31 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <pthread.h>
 
+void* hilo1(void* arg) {
+    printf("Soy el hilo 1\n");
+    pthread_exit(NULL);
+}
 
-void *funcion_hilo(void *arg) {
-    printf("¡Hola desde el hilo %ld!\n", (long)arg);
+void* hilo2(void* arg) {
+    printf("Soy el hilo 2\n");
+    pthread_exit(NULL);
+}
+
+void* hilo3(void* arg) {
+    printf("Soy el hilo 3\n");
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t hilo1, hilo2, hilo3;
-    long id_hilo1 = 1, id_hilo2 = 2, id_hilo3 = 3;
-    
-    pthread_create(&hilo1, NULL, funcion_hilo, (void *)id_hilo1);
-    pthread_create(&hilo2, NULL, funcion_hilo, (void *)id_hilo2);
-    pthread_create(&hilo3, NULL, funcion_hilo, (void *)id_hilo3);
-    
-    pthread_join(hilo1, NULL);
-    pthread_join(hilo2, NULL);
-    pthread_join(hilo3, NULL);
-    
-    printf("Los hilos han terminado\n");
+    pthread_t t1, t2, t3;
+
+    pthread_create(&t1, NULL, hilo1, NULL);
+    pthread_create(&t2, NULL, hilo2, NULL);
+    pthread_create(&t3, NULL, hilo3, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
     return 0;
 }
